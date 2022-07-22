fee4d75049400c61c87f4014ece07595327590ec
shoaib <shoaib.doulatabad@gmail.com>
2022-07-23 01:24:20 +0400

    intial setup with create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill">Response</i>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
